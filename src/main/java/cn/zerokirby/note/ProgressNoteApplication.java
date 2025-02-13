2b5142725af5810e44a09143ab1edceef2824ac1
0Kirby <jty.1234567890@163.com>
2025-02-14 00:48:03 +0800

    initialize Spring Boot project with Gradle setup and basic configuration

---
+++ src/main/java/cn/zerokirby/note/ProgressNoteApplication.java
@@ -0,0 +1,13 @@
+package cn.zerokirby.note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgressNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/zerokirby/note/ProgressNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zerokirby.note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
